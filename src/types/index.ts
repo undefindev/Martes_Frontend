342cb331e20cece9852bc6db2f51575123e2d598
alex bedolla <undefindev@outlook.com>
2024-05-03 07:39:57 -0700

    utilizando 'zod' para asignar los types al formulario

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod'
+
+/* project */
+export const projectSchema = z.object({
+  _id: z.string(),
+  projectName: z.string(),
+  clientName: z.string(),
+  description: z.string()
+})
+export type Project = z.infer<typeof projectSchema>
+export type ProjectFormData = Pick<Project, 'projectName' | 'clientName' | 'description'>
