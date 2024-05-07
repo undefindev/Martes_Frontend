7cc0895e5cba872292ce8bddaa8c79978ba8e600
alex bedolla <undefindev@outlook.com>
2024-05-06 21:12:26 -0700

    intalndo 'toastify' y 'react-query'

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,28 +1,39 @@
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
 import Logo from "@/components/Logo";
 import NavMenu from "@/components/NavMenu";
 
 export default function AppLayout() {
   return (
     <>
-      <header className="py-2">
-        <div className="container mx-auto flex flex-col lg:flex-row justify-between items-center">
-          <div className="w-32">
-            <Logo />
+      <div className="grid grid-rows-3">
+        <header className="py-2">
+          <div className="container mx-auto flex flex-col lg:flex-row justify-between items-center">
+            <div className="w-32">
+              <Logo />
+            </div>
+            <NavMenu />
           </div>
-          <NavMenu />
-        </div>
-      </header>
-      <section className="container mx-auto mt-8 p-4">
-        <Outlet />
-      </section>
-      <footer className="py-4">
-        <div className="flex flex-col items-center">
-          <p>kitchen by.. a la huevona..!!</p>
-          <p>todos los derechos reservados alimentate sanamente come frutas y verduras {new Date().getFullYear()}</p>
-        </div>
-      </footer>
+        </header>
+        <section className="container mx-auto mt-8 p-4">
+          <div className="grid grid-cols-2">
+            <div>sidebar</div>
+            <Outlet />
+          </div>
+        </section>
+        <footer className="py-4">
+          <div className="flex flex-col items-center">
+            <p>kitchen by.. a la huevona..!!</p>
+            <p>todos los derechos reservados alimentate sanamente come frutas y verduras {new Date().getFullYear()}</p>
+          </div>
+        </footer>
 
+        <ToastContainer
+          pauseOnHover={false}
+          pauseOnFocusLoss={false}
+        />
+      </div>
     </>
   )
 }
--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from "react-router-dom"
 import { useForm } from 'react-hook-form'
+import { toast } from 'react-toastify'
 import ProjectForm from "@/components/projects/ProjectForm"
 import { ProjectFormData } from "@/types/index"
 import { createProject } from '@/api/ProjectAPI'
@@ -15,8 +16,9 @@ export default function CreateProjectView() {
 
   const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
 
-  const handleForm = async (data: ProjectFormData) => {
-    await createProject(data) // creame el proyecto..
+  const handleForm = async (formData: ProjectFormData) => {
+    const data = await createProject(formData) // creame el proyecto..
+    toast.success(data)
     navigate('/') // y regresamelo a la vrga a la pagina principal
   }
   return (
