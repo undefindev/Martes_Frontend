b3c4e0249eec6df053ae3296f28a9cc3a63f36f5
alex bedolla <undefindev@outlook.com>
2024-05-13 21:23:49 -0700

    instalamos React-Query-Devtolls y morgan

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './index.css'
 import Router from './router'
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryCLient}>
       <Router />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>,
 )
