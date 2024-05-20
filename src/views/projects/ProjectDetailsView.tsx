af79a0e6f38471d633332cc1527be745ab440f94
alex bedolla <undefindev@outlook.com>
2024-05-19 21:05:23 -0700

    Mostarndo Los Estados De Las Tareas

--- src/views/projects/ProjectDetailsView.tsx
+++ src/views/projects/ProjectDetailsView.tsx
@@ -2,6 +2,7 @@ import { Navigate, useNavigate, useParams } from "react-router-dom"
 import { useQuery } from "@tanstack/react-query"
 import { getProjectById } from "@/api/ProjectAPI"
 import AddTaskModal from "@/components/tasks/AddTaskModal"
+import TaskList from "@/components/tasks/TaskList"
 
 export default function ProjectDetailsView() {
 
@@ -30,6 +31,11 @@ export default function ProjectDetailsView() {
           className="border border-slate-500 rounded-lg py-2 px-4 hover:bg-slate-900 hover:text-white cursor-pointer transition-colors"
         >Agregar Tarea</button>
       </nav>
+
+      <TaskList
+        tasks={data.tasks}
+      />
+
 
       <AddTaskModal />
     </>
