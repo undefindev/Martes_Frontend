c44a58a5b46e779ffca26f43ef99dd99d9cea5aa
alex bedolla <undefindev@outlook.com>
2024-07-10 23:10:22 -0700

    mostrando el nombre del usuario en el menu

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -2,8 +2,13 @@ import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 import { Bars3Icon } from '@heroicons/react/20/solid'
 import { Link } from 'react-router-dom'
+import { User } from '../types'
 
-export default function NavMenu() {
+type NavMenueProps = {
+  name: User['name']
+}
+
+export default function NavMenu({ name }: NavMenueProps) {
 
   return (
     <Popover className="relative">
@@ -22,7 +27,7 @@ export default function NavMenu() {
       >
         <Popover.Panel className="absolute left-1/2 z-10 mt-5 flex w-screen lg:max-w-min -translate-x-1/2 lg:-translate-x-48">
           <div className="w-full lg:w-56 shrink rounded-xl p-4 text-sm font-semibold leading-6 shadow-lg ring-1">
-            <p className='text-center'>Hola: Usuario</p>
+            <p className='text-center'>Hola: {name}</p>
             <Link
               to='/profile'
               className='block p-2 hover:text-purple-950'
