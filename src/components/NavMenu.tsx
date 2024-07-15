ca8e8695131e1c06182f93ab8c26eac79d5fdbd6
alex bedolla <undefindev@outlook.com>
2024-07-14 19:36:17 -0700

    el menu no se miraba por ensima.. z-index-10 to z-index-20

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,6 +1,5 @@
 import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
-import { RxAvatar } from "react-icons/rx";
 import { Link } from 'react-router-dom'
 import { User } from '../types'
 import { useQueryClient } from '@tanstack/react-query'
@@ -37,7 +36,7 @@ export default function NavMenu({ name }: NavMenueProps) {
         leaveFrom="opacity-100 translate-y-0"
         leaveTo="opacity-0 translate-y-1"
       >
-        <Popover.Panel className="absolute left-1/2 z-10 mt-5 flex w-screen lg:max-w-min -translate-x-1/2 lg:-translate-x-48">
+        <Popover.Panel className="absolute bg-slate-50 left-1/2 z-20 mt-5 flex w-screen lg:max-w-min -translate-x-1/2 lg:-translate-x-48">
           <div className="w-full lg:w-56 shrink rounded-xl p-4 text-sm font-semibold leading-6 shadow-lg ring-1">
             <p className='text-center'>Hola: {name}</p>
             <Link
