098cf035d6ef1682bd6ae1249a9f6b79cca63945
alex bedolla <undefindev@outlook.com>
2024-07-24 23:06:41 -0700

    agregamos el copyrigt symbol al footer.. no mas de pura mamada

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <>
       <p className="text-center">
-        Martes.org - by a la huevona..!! Todos los derechos reservados {new Date().getFullYear()}
+        Martes.org&copy; - by a la huevona..!!&copy; Todos los derechos reservados {new Date().getFullYear()}
       </p>
     </>
   )
