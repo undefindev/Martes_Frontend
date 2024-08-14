3456aaa167a5570558bea987f608944a98939e20
alex bedolla <undefindev@outlook.com>
2024-08-14 07:39:37 -0700

    useAuth, useMemo, y creando el boton de eliminar la nota

--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -1,19 +1,29 @@
+import { useAuth } from "@/hooks/useAuth"
 import { Note } from "@/types/index"
 import { formatDate } from "@/utils/utils"
+import { useMemo } from "react"
+import { LuTrash2 } from "react-icons/lu"
 
 type NoteDetailProps = { // esto es como cuando destructuramos las props en react normalmente.. aahhh ya entendi. por fin pendejo..!!
   note: Note
 }
 export default function NoteDetail({ note }: NoteDetailProps) {
+
+  const { data, isLoading } = useAuth()
+  const canDelete = useMemo(() => data?._id === note.createdBy._id, [data])
+
+  if (isLoading) return 'Cargando..'
+
   return (
-    <div className="p-2 flex place-items-baseline  justify-between">
-      <p>
-        {note.content}
-      </p>
-      <div className="flex items-baseline gap-2">
-        <p className="text-xs text-gray-500">{formatDate(note.createdAt)}{' '}Por:{' '}</p>
-        <p className="text-sm">{note.createdBy.name}</p>
+    <div className="p-2 flex items-center justify-between">
+      <div>
+        <p>{note.content} Por: <span>{note.createdBy.name}</span></p>
+        <p className="text-xs text-gray-500">{formatDate(note.createdAt)}</p>
       </div>
+
+      {canDelete && (
+        <button type="button" className="text-red-400 hover:text-red-500 cursor-pointer transition-colors"><LuTrash2 /></button>
+      )}
     </div>
   )
 }
