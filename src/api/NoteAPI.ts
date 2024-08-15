a0da0b6d52e2e350723fd751ef13317c37b36e48
alex bedolla <undefindev@outlook.com>
2024-08-14 21:06:05 -0700

    Eliminando las Notas e Invalidando los queries

--- src/api/NoteAPI.ts
+++ src/api/NoteAPI.ts
@@ -1,11 +1,12 @@
 import { isAxiosError } from "axios";
-import { NoteFormData, Project, Task } from "../types";
+import { Note, NoteFormData, Project, Task } from "../types";
 import api from "@/lib/axios";
 
 type NoteAPIType = {
   formData: NoteFormData
   projectId: Project['_id']
   taskId: Task['_id']
+  noteId: Note['_id']
 
 }
 
@@ -18,5 +19,18 @@ export async function createNote({ projectId, taskId, formData }: Pick<NoteAPITy
     if (isAxiosError(error) && error.response) {
       throw new Error(error.response.data.error)
     }
+  }
+}
+
+export async function deleteNote({ projectId, taskId, noteId }: Pick<NoteAPIType, 'projectId' | 'taskId' | 'noteId'>) {
+  try {
+    const url = `/projects/${projectId}/tasks/${taskId}/notes/${noteId}`
+    const { data } = await api.delete<string>(url) // esta es la peticion
+    return data
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
+
   }
 }
--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -1,8 +1,12 @@
+import { deleteNote } from "@/api/NoteAPI"
 import { useAuth } from "@/hooks/useAuth"
 import { Note } from "@/types/index"
 import { formatDate } from "@/utils/utils"
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useMemo } from "react"
 import { LuTrash2 } from "react-icons/lu"
+import { useLocation, useParams } from "react-router-dom"
+import { toast } from "react-toastify"
 
 type NoteDetailProps = { // esto es como cuando destructuramos las props en react normalmente.. aahhh ya entendi. por fin pendejo..!!
   note: Note
@@ -10,7 +14,23 @@ type NoteDetailProps = { // esto es como cuando destructuramos las props en reac
 export default function NoteDetail({ note }: NoteDetailProps) {
 
   const { data, isLoading } = useAuth()
-  const canDelete = useMemo(() => data?._id === note.createdBy._id, [data])
+  const canDelete = useMemo(() => data?._id === note.createdBy._id, [data?._id, note.createdBy._id]) // ta raro esto.. 
+  const params = useParams()
+  const location = useLocation()
+  const queryParams = new URLSearchParams(location.search)
+
+  const projectId = params.projectId! // '!' de TypeScript para asegurar que la propiedad projectId de params no es nula ni undefined
+  const taskId = queryParams.get('viewTask')!
+
+  const queryClient = useQueryClient()
+  const { mutate } = useMutation({
+    mutationFn: deleteNote,
+    onError: (error) => toast.error(error.message),
+    onSuccess: (data) => {
+      toast.success(data)
+      queryClient.invalidateQueries({ queryKey: ['task', taskId] })
+    }
+  })
 
   if (isLoading) return 'Cargando..'
 
@@ -22,7 +42,12 @@ export default function NoteDetail({ note }: NoteDetailProps) {
       </div>
 
       {canDelete && (
-        <button type="button" className="text-red-400 hover:text-red-500 cursor-pointer transition-colors"><LuTrash2 /></button>
+        <button
+          type="button"
+          className="text-red-400 hover:text-red-500 cursor-pointer transition-colors"
+          onClick={() => mutate({ projectId, taskId, noteId: note._id })}>
+          <LuTrash2 />
+        </button>
       )}
     </div>
   )
