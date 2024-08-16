5a9e41d52763fa78f31eab64ad943d12408f7832
alex bedolla <undefindev@outlook.com>
2024-08-15 22:01:55 -0700

    routing para las rutas de perfil

--- src/layouts/ProfileLayout.tsx
+++ src/layouts/ProfileLayout.tsx
@@ -1,10 +1,11 @@
+import Tabs from "@/components/profile/Tabs";
 import { Outlet } from "react-router-dom";
 
 
 export default function ProfileLayout() {
   return (
     <>
-      <div>ProfileLayout</div>
+      <Tabs />
       <Outlet />
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-import colors from "tailwindcss/colors"
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -6,19 +6,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        secondary: {
-          DEFAULT: colors.neutral[100],
-          hover: colors.stone[200],
-          border: colors.stone[300],
-          text: colors.stone[400],
-          dark: colors.stone[600],
-          ["dark-hover"]: colors.stone[800]
-        }
-      }
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    // ...
+  ],
 }
 
